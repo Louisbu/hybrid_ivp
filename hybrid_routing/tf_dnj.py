a21b7059074e968fc9b782b97a89c8ce86af78a0
ShrimpyLouis <louisbu2001@icloud.com>
2022-07-26 05:27:03 +0200

    hybridrouting w/ tf done, ODEsolver not working

---
+++ hybrid_routing/tf_dnj.py
@@ -0,0 +1,70 @@
+import jax.numpy as jnp
+from jax import grad, jacfwd, jacrev, jit
+
+from hybrid_routing.benchmark import background_vector_field
+
+
+def cost_function(x, xp):
+
+    w = background_vector_field(x[0], x[1])
+    cost = 0.5 * ((xp[0] - w[0]) ** 2 + (xp[1] - w[1]) ** 2)
+    return cost
+
+
+def discretized_cost_function(q0, q1, h):
+    L1 = cost_function(q0, (q1 - q0) / h)
+    L2 = cost_function(q1, 1 / h * (q1 - q0))
+    L_d = h / 2 * (L1 + L2)
+    return L_d
+
+
+def hessian(f, argnums=0):
+    return jacfwd(jacrev(f, argnums=argnums), argnums=argnums)
+
+
+D1Ld = jit(grad(discretized_cost_function, argnums=0))
+D2Ld = jit(grad(discretized_cost_function, argnums=1))
+D11Ld = jit(hessian(discretized_cost_function, argnums=0))
+D22Ld = jit(hessian(discretized_cost_function, argnums=1))
+
+
+def optimize_distance(pts, T, N, n):
+    damping = 0.9  # ~1
+
+    h = T / (N - 1)
+
+    # Implementar el método para la trayectoria.
+    x = jnp.array(pts)
+    A = []
+    A.append(x[0])
+    for k in range(n):
+        for l in range(1, N - 1):
+            qkm1 = jnp.array(x[l - 1])
+
+            qk = jnp.array(x[l])
+
+            qkp1 = jnp.array(x[l + 1])
+
+            b = -D2Ld(qkm1, qk, h) - D1Ld(qk, qkp1, h)
+            a = D22Ld(qkm1, qk, h) + D11Ld(qk, qkp1, h)
+
+            Q = jnp.linalg.solve(a, b)
+
+            A.append(damping * Q + x[l])
+        x = jnp.array(A)
+        A = [x[0]]
+    return x
+
+
+def main():
+    x0 = jnp.array([0.0, 0.0])
+    xN = jnp.array([6.0, 5.0])
+    T = 30
+    N = 50
+    n = 20
+    x = optimize_distance(x0, xN, T, N, n)
+    print([x, xN])
+
+
+if __name__ == "__main__":
+    main()
---
+++ hybrid_routing/tf_zivp.py
@@ -0,0 +1,66 @@
+import numpy as np
+from jax import vmap, jacfwd, jacrev, jit
+import jax.numpy as jnp
+from hybrid_routing.tf_benchmark import background_vector_field
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+
+def tf_dvdx(x, y):
+    dvx = jit(jacrev(background_vector_field, argnums=1))
+    return dvx(x, y)[0]
+
+
+def tf_dvdy(x, y):
+    dvy = jit(jacrev(background_vector_field, argnums=1))
+    return dvy(x, y)[1]
+
+
+def tf_dudx(x, y):
+    dux = jit(jacfwd(background_vector_field, argnums=0))
+    return dux(x, y)[0]
+
+
+def tf_dudy(x, y):
+    duy = jit(jacfwd(background_vector_field, argnums=0))
+    return duy(x, y)[1]
+
+# jacobian of the vector field must be declared inside the tensorflow "with" environment
+def tf_wave(p, t, vel=0.5):
+    x, y, theta = tf.constant(p)
+    vector_field = background_vector_field(x, y)
+    dxdt = vel * tf.cos(theta) + vector_field[0]
+    dydt = vel * tf.sin(theta) + vector_field[1]
+    vector_field = background_vector_field(x,y)
+    with tf.GradientTape(persistent=True) as tape:
+        tape.watch(x)
+        tape.watch(y)
+        x0 = tf.cos(2 * x - y - 6)
+        x1 = 2 / 3 * tf.sin(y) + x - 3
+    du = tape.jacobian(x0, [x,y])
+    dv = tape.jacobian(x1, [x,y])
+    print(du, dv)
+    dthetadt = (
+        du[1] * tf.sin(theta) ** 2
+        + tf.sin(theta) * tf.cos(theta) * (du[0] - dv[1])
+        - dv[0] * tf.cos(theta) ** 2
+    )
+    return [dxdt, dydt, dthetadt]
+
+print(tf_wave([1., 1., 0.2], np.linspace(0,10,1)))
+
+def dist_to_dest(p0, p1):
+    return np.sqrt((p0[0] - p1[0]) ** 2 + (p0[1] - p1[1]) ** 2)
+
+
+def min_dist_to_dest(candidates, pN):
+    dist = []
+    min_dist = dist_to_dest(candidates[0], pN)
+    best_point = candidates[0]
+    for i in range(len(candidates)):
+        dist = dist_to_dest(candidates[i], pN)
+        if dist < min_dist:
+            min_dist = dist
+            best_point = candidates[i]
+    return best_point
+
