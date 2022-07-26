39ec2a81b69e7962644d605966c16db64a4c0410
daniprec <daniel.precioso@uca.es>
2022-07-26 19:03:29 +0200

    Use tf operations

--- hybrid_routing/tensorflow/benchmark.py
+++ hybrid_routing/tensorflow/benchmark.py
@@ -1,20 +1,33 @@
 import tensorflow as tf
 
 
-def R(a, b, x, y):
-    coeff = 1 / (3 * ((x - a) ** 2 + (y - b) ** 2) + 1)
-    R = (coeff * (-y + b), coeff * (x - a))
-    return R
+C1 = tf.constant(1.0, dtype=tf.float32)
+C2 = tf.constant(2.0, dtype=tf.float32)
+C3 = tf.constant(3.0, dtype=tf.float32)
+C4 = tf.constant(4.0, dtype=tf.float32)
+C5 = tf.constant(5.0, dtype=tf.float32)
+
+
+def R(a: tf.Tensor, b: tf.Tensor, x: tf.Tensor, y: tf.Tensor) -> tf.Tensor:
+    coeff = tf.math.divide(
+        C1,
+        tf.math.multiply(
+            C3,
+            (tf.math.pow(x - a, 2) + tf.math.pow(y - b, 2)),
+        )
+        + C1,
+    )
+    return (tf.math.multiply(coeff, -y + b), tf.math.multiply(coeff, x - a))
 
 
 @tf.function
-def background_vector_field(x: tf.constant, y: tf.constant) -> tf.constant:
-    field = 1.7 * (
-        tf.math.negative(R(2, 2, x, y))
-        + tf.math.negative(R(4, 4, x, y))
-        + tf.math.negative(R(2, 5, x, y))
-        + R(5, 1, x, y)
+def background_vector_field(x: tf.Tensor, y: tf.Tensor) -> tf.Tensor:
+
+    field = tf.math.multiply(
+        tf.constant(1.7, dtype=tf.float32),
+        tf.math.negative(R(C2, C2, x, y))
+        + tf.math.negative(R(C4, C4, x, y))
+        + tf.math.negative(R(C2, C5, x, y))
+        + R(C5, C1, x, y),
     )
-    x = field[0]
-    y = field[1]
-    return (x, y)
+    return (field[0], field[1])
--- hybrid_routing/tensorflow/zivp.py
+++ hybrid_routing/tensorflow/zivp.py
@@ -26,26 +26,30 @@ def tf_dudy(x, y):
 
 
 # jacobian of the vector field must be declared inside the tensorflow "with" environment
-def tf_wave(t, p, vel=1):
-    x, y, theta = tf.constant(p)
-    vector_field = background_vector_field(x, y)
-    dxdt = vel * tf.cos(theta) + vector_field[0]
-    dydt = vel * tf.sin(theta) + vector_field[1]
+def tf_wave(
+    t: tf.Tensor, p: tf.Tensor, vel: tf.Tensor = tf.constant(1.0, dtype=tf.float32)
+) -> tf.Tensor:
+    x, y, theta = p
 
     with tf.GradientTape(persistent=True) as tape:
         tape.watch(x)
         tape.watch(y)
         x0, x1 = background_vector_field(x, y)
+
+    dxdt = tf.math.multiply(tf.cos(theta), vel) + x0
+    dydt = tf.math.multiply(tf.sin(theta), vel) + x1
+
     du = tape.jacobian(x0, [x, y])
     dv = tape.jacobian(x1, [x, y])
     dthetadt = (
-        du[1] * tf.sin(theta) ** 2
-        + tf.sin(theta) * tf.cos(theta) * (du[0] - dv[1])
-        - dv[0] * tf.cos(theta) ** 2
+        tf.math.multiply(du[1], tf.math.pow(tf.sin(theta), 2))
+        + tf.math.multiply(
+            tf.math.multiply(tf.sin(theta), tf.cos(theta)), du[0] - dv[1]
+        )
+        - tf.math.multiply(dv[0], tf.math.pow(tf.cos(theta), 2))
     )
-    # print(type(dxdt))
-    p = tf.convert_to_tensor([dxdt, dydt, dthetadt], dtype=tf.float32)
-    return tf.constant(p)
+    p_new = tf.convert_to_tensor([dxdt, dydt, dthetadt], dtype=tf.float32)
+    return tf.constant(p_new)
 
 
 def dist_to_dest(p0, p1):
@@ -65,6 +69,6 @@ def min_dist_to_dest(candidates, pN):
 
 
 if __name__ == "__main__":
-    p = [1.0, 1.0, 0.2]
-    t = np.linspace(0, 10, 1)
+    p = tf.constant([1.0, 1.0, 0.2], dtype=tf.float32)
+    t = tf.constant(np.linspace(0, 10, 1), dtype=tf.float32)
     print(tf_wave(t, p))
