c7ed5a4a7cdf58d9435a825d1aa62578e9098473
ShrimpyLouis <louisbu2001@icloud.com>
2022-07-27 16:08:25 +0200

    created constant current

---
+++ hybrid_routing/vectorfields/constant_current.py
@@ -0,0 +1,10 @@
+from hybrid_routing.vectorfields.base import Vectorfield
+import jax.numpy as jnp
+
+
+class ConstantCurrent(Vectorfield):
+    def __init__(self):
+        pass
+
+    def get_current(self, x, y):
+        return jnp.asarray([1.0, 1.0])
