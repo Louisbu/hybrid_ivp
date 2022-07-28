97ed59689adb2bd6fb1e053005df2d0f4301adc7
ShrimpyLouis <louisbu2001@icloud.com>
2022-07-28 15:47:18 +0200

    added circular vector field

--- hybrid_routing/vectorfields/__init__.py
+++ hybrid_routing/vectorfields/__init__.py
@@ -3,4 +3,6 @@ from hybrid_routing.vectorfields.constant_current import ConstantCurrent
 from hybrid_routing.vectorfields.saddle import Saddle
 from hybrid_routing.vectorfields.sink import Sink
 from hybrid_routing.vectorfields.source import Source
-from hybrid_routing.vectorfields.hill_bowl import HillBowl
+from hybrid_routing.vectorfields.circular import Circular
+
+# from hybrid_routing.vectorfields.hill_bowl import HillBowl
