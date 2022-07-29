7be2f83d44248132c3b79d24d1b1b4151a059d1c
daniprec <daniel.precioso@uca.es>
2022-07-29 20:16:50 +0200

    Vectorize DNJ computation

--- hybrid_routing/jax_utils/dnj.py
+++ hybrid_routing/jax_utils/dnj.py
@@ -4,7 +4,7 @@ from typing import Callable
 import jax.numpy as jnp
 from hybrid_routing.vectorfields.base import Vectorfield
 from hybrid_routing.vectorfields.constant_current import ConstantCurrent
-from jax import grad, jacfwd, jacrev, jit
+from jax import grad, jacfwd, jacrev, jit, vmap
 from pyparsing import Iterable
 
 
@@ -13,27 +13,34 @@ def hessian(f: Callable, argnums: int = 0):
 
 
 class DNJ:
-    def __init__(self, vectorfield: Vectorfield) -> None:
+    def __init__(self, vectorfield: Vectorfield, time_step: float = 0.1) -> None:
+        self.time_step = time_step
+        h = time_step
+
         def cost_function(x: jnp.array, xp: jnp.array) -> Iterable[float]:
             w = vectorfield.get_current(x[0], x[1])
             cost = 0.5 * ((xp[0] - w[0]) ** 2 + (xp[1] - w[1]) ** 2)
             return cost
 
-        def discretized_cost_function(
-            q0: jnp.array, q1: jnp.array, h: float
-        ) -> Iterable[float]:
+        def discretized_cost_function(q0: jnp.array, q1: jnp.array) -> Iterable[float]:
             L1 = cost_function(q0, (q1 - q0) / h)
             L2 = cost_function(q1, 1 / h * (q1 - q0))
             L_d = h / 2 * (L1**2 + L2**2)
             return L_d
 
+        D1Ld = grad(discretized_cost_function, argnums=0)
+        D2Ld = grad(discretized_cost_function, argnums=1)
+        D11Ld = hessian(discretized_cost_function, argnums=0)
+        D22Ld = hessian(discretized_cost_function, argnums=1)
+
+        def optimize(qkm1: jnp.array, qk: jnp.array, qkp1: jnp.array) -> jnp.array:
+            b = -D2Ld(qkm1, qk) - D1Ld(qk, qkp1)
+            a = D22Ld(qkm1, qk) + D11Ld(qk, qkp1)
+            return jnp.linalg.solve(a, b)
+
         self.cost_function = cost_function
         self.discretized_cost_function = discretized_cost_function
-
-        self.D1Ld = jit(grad(discretized_cost_function, argnums=0))
-        self.D2Ld = jit(grad(discretized_cost_function, argnums=1))
-        self.D11Ld = jit(hessian(discretized_cost_function, argnums=0))
-        self.D22Ld = jit(hessian(discretized_cost_function, argnums=1))
+        self.optim_vect = vmap(optimize, in_axes=(0, 0, 0), out_axes=0)
 
     def __hash__(self):
         return hash(())
@@ -41,45 +48,21 @@ class DNJ:
     def __eq__(self, other):
         return isinstance(other, DNJ)
 
-    @partial(jit, static_argnums=(0, 2, 3))
-    def optimize_distance(
-        self,
-        pts: jnp.array,
-        t_total: float,
-        damping: float = 0.9,
-    ) -> jnp.array:
-        num_points = len(pts)
-        h = t_total / (num_points - 1)
-        # Implement method for the trajectory
-        x = jnp.copy(pts)
-        x_new = jnp.copy(x)
-        for idx in range(1, num_points - 1):
-            qkm1 = x[idx - 1]
-            qk = x[idx]
-            qkp1 = x[idx + 1]
-
-            b = -self.D2Ld(qkm1, qk, h) - self.D1Ld(qk, qkp1, h)
-            a = self.D22Ld(qkm1, qk, h) + self.D11Ld(qk, qkp1, h)
-
-            Q = jnp.linalg.solve(a, b)
-
-            x_new = x_new.at[idx].set(damping * Q + qk)
-        return x_new
+    @partial(jit, static_argnums=(0, 2))
+    def optimize_distance(self, pts: jnp.array, damping: float = 0.9) -> jnp.array:
+        pts_new = jnp.copy(pts)
+        q = self.optim_vect(pts[:-2], pts[1:-1], pts[2:])
+        return pts_new.at[1:-1].set(damping * q + pts[1:-1])
 
 
 def main():
     x0 = jnp.array([[0.0, 0.0], [1.0, 1.0], [2.0, 2.0], [3.0, 3.0]])
-    t_total = 30
-    num_points = 10
-    n_iter = 10
     dnj = DNJ(vectorfield=ConstantCurrent())
 
-    print("Cost\n", dnj.cost_function(x0, x0))
-    print("\nDiscretize\n", dnj.discretized_cost_function(x0, x0, 0.5))
-    print(x0)
-    print("\nDerivative\n", dnj.D2Ld(x0, x0, 0.5))
+    print("Cost\n", dnj.cost_function(x0, x0).shape)
+    print("\nDiscretize\n", dnj.discretized_cost_function(x0, x0).shape)
 
-    x = dnj.optimize_distance(x0, t_total, num_points, n_iter)
+    x = dnj.optimize_distance(x0)
     print(x)
 
 
