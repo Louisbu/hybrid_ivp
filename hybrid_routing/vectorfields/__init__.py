cbbf244c6bdfc0bb7dd2c89895fb8854bb6a5a24
ShrimpyLouis <louisbu2001@icloud.com>
2022-08-03 17:52:14 +0200

    included vectorfield described in paper

--- hybrid_routing/vectorfields/__init__.py
+++ hybrid_routing/vectorfields/__init__.py
@@ -4,6 +4,6 @@ from hybrid_routing.vectorfields.hill_bowl import HillBowl
 from hybrid_routing.vectorfields.no_current import NoCurrent
 from hybrid_routing.vectorfields.sink import Sink
 from hybrid_routing.vectorfields.source import Source
+from hybrid_routing.vectorfields.four_vortices import FourVortices
 
 # from hybrid_routing.vectorfields.saddle import Saddle
-# from hybrid_routing.vectorfields.four_vortices import FourVortices
