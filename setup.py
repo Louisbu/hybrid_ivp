a89ecadd15c956ac423ff332451ff99d015ac6f0
daniprec <daniel.precioso@uca.es>
2022-08-03 15:55:14 +0200

    Add black to setup and requirements

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
     python_requires=">=3.9",
     setup_requires=["setuptools_scm"],
     install_requires=[
+        "black",
         "jax==0.3.14",
         "jaxlib==0.3.14",
         "matplotlib",
