b2c3d98204601743ed16d1f35e2e26ffe17c6107
daniprec <daniel.precioso@uca.es>
2022-08-03 19:47:45 +0200

    Update test vectorfields

--- pytest/test_vectorfield.py
+++ pytest/test_vectorfield.py
@@ -1,11 +1,20 @@
 import jax.numpy as jnp
-from hybrid_routing.vectorfields import ConstantCurrent, NoCurrent
+from hybrid_routing.vectorfields import Circular, NoCurrent
 
-
-def test_constant_current_vectorfield():
-    vectorfield = ConstantCurrent()
-    assert (vectorfield.get_current(0, 0) == jnp.asarray([0.2, -0.2])).all()
 
 def test_no_current_vectorfield():
     vectorfield = NoCurrent()
     assert (vectorfield.get_current(0, 0) == jnp.asarray([0, 0])).all()
+    assert vectorfield.dudx(0, 0) == 0
+    assert vectorfield.dvdx(0, 0) == 0
+    assert vectorfield.dudy(0, 0) == 0
+    assert vectorfield.dvdy(0, 0) == 0
+
+
+def test_circular_vectorfield():
+    vectorfield = Circular()
+    assert (vectorfield.get_current(0, 0) == jnp.asarray([0.05, -0.15])).all()
+    assert vectorfield.dudx(0, 0) == 0
+    assert vectorfield.dvdx(0, 0) == -0.05
+    assert vectorfield.dudy(0, 0) == 0.05
+    assert vectorfield.dvdy(0, 0) == 0
