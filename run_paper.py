e0bae1f3ba91c7750c51e4a1db3729404b4ba861
daniprec <daniel.precioso@uca.es>
2022-12-31 13:26:37 +0100

    Build output folder for paper

--- run_paper.py
+++ run_paper.py
@@ -2,11 +2,16 @@
 Generate all the figures used in the paper
 """
 
+from pathlib import Path
+
 import typer
 
 
-def main():
-    pass
+def main(path_out: str = "output"):
+    # Build output folder
+    path_out: Path = Path(path_out)
+    if not path_out.exists():
+        path_out.mkdir()
 
 
 if __name__ == "__main__":
